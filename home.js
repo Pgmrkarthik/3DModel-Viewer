d0afc6c52012482c6ee825f9e182593e2ebca87c
Karthikeyan k <pgmrkarthik@gmail.com>
2020-02-28 11:11:34 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+
+window.onload=function(){
+var modelContainer = document.getElementsByClassName('gallary');
+console.log(modelContainer)
+modelContainer.onclick=function(event){
+    console.log(event);
+}
+}
---
+++ main.js
@@ -0,0 +1,35 @@
+
+window.onload = function(){
+  document.getElementById('fileInput').addEventListener('change', function () {
+    if (this.files && this.files[0]) {
+      if ( document.getElementById("model-viewer")) {
+        document.getElementById("model-viewer").remove();
+      }
+      var modelNameTag =document.getElementById("model-name");
+      var modelName = this.files[0].name;
+      modelName = modelName.substring(0,modelName.indexOf('.'))
+      modelNameTag.innerText = modelName
+      var Model2 = document.getElementById("dmodel-viewer")
+      var model = URL.createObjectURL(this.files[0]);
+     var model_viewertag = '<model-viewer ar src="' + model + '" shadow-intensity="1" auto-rotate camera-controls alt=""></model-viewer>'
+     //var model_viewertag = '<model-viewer ar src="NewModem.glb" shadow-intensity="1" auto-rotate camera-controls alt="" ar-scale="auto" ios-src="Models/1Modem.usdz"></model-viewer>'
+      Model2.innerHTML = model_viewertag;
+    }
+  })
+  // document.addEventListener('ondrop', function (event) {
+  //  var droppedFiles = event.target.files || event.dataTransfer.files;
+  //   if (droppedFiles && droppedFiles[0]) {
+  //     if ( document.getElementById("model-viewer")) {
+  //       document.getElementById("model-viewer").remove();
+  //     }
+  //     var modelNameTag =document.getElementById("model-name");
+  //     var modelName = files[0].name;
+  //     modelName = modelName.substring(0,modelName.indexOf('.'))
+  //     modelNameTag.innerText = modelName
+  //     var Model2 = document.getElementById("dmodel-viewer")
+  //     var model = URL.createObjectURL(droppedFiles[0]);
+  //     var model_viewertag = '<model-viewer ar src="' + model + '" shadow-intensity="1" auto-rotate camera-controls alt=""></model-viewer>'
+  //     Model2.innerHTML = model_viewertag;
+  //   }
+  // })
+}
